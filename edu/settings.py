05ced01a46199a940fac70b7622b0a07470e1f11
Aravind R Nair <121355701+ardr000005@users.noreply.github.com>
2024-10-19 06:26:31 +0530

    Update settings.py

--- edu/settings.py
+++ edu/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['sustainable-scholars.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['sustainable-scholars.onrender.com', 'sustainable-scholars-1.onrender.com']
 
 AUTH_USER_MODEL = 'school.School'
 
